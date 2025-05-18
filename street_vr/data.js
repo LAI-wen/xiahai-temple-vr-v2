9380b7412ca8b5f6d4c4e4863708badbdaac656b
LAI-wen <gigilai1688@gmail.com>
2025-05-18 10:06:52 +0800

    first commit

---
+++ street_vr/data.js
@@ -0,0 +1,506 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "金紙鋪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7227408565492652,
+          "pitch": 0.48927312228719977,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.8524321627744662,
+          "pitch": 0.3560862820778894,
+          "rotation": 0,
+          "target": "5-1jpg"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "側廳義勇公",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9838509113983136,
+          "pitch": 0.48684351840836015,
+          "rotation": 0,
+          "target": "5-1jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3874633875372897,
+          "pitch": 0.5808722349897728,
+          "title": "<span id=\"docs-internal-guid-b20abafb-7fff-2ca0-c39d-01700d47c6ed\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"7616583\" _msthash=\"264\">義勇公</span></span>",
+          "text": "<span id=\"docs-internal-guid-b09d7ddb-7fff-6858-d204-342169a8e7c3\" _msttexthash=\"1277205410\" _msthash=\"265\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">義勇公</span><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">為咸豐年間保護城隍神像殉難的三十八位先烈，配祀虎爺、馬使爺、施琅與廖添丁等神，展現大稻埕在地歷史與義勇精神，是城隍廟中最具忠誠與地方色彩的信仰空間。</span></span>"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "點香處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.4009604647499483,
+          "pitch": 0.44161838309282153,
+          "rotation": 0,
+          "target": "9-1"
+        },
+        {
+          "yaw": -2.5381319875246255,
+          "pitch": 0.3573603674377317,
+          "rotation": 0,
+          "target": "8-2"
+        },
+        {
+          "yaw": -0.9582494181772834,
+          "pitch": 0.2500142577759661,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-1_2",
+      "name": "側廳1_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.719664440890421,
+          "pitch": 0.5303387461523528,
+          "rotation": 0,
+          "target": "5-1jpg"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.012490055052996496,
+          "pitch": 0.4233476580498561,
+          "title": "<span id=\"docs-internal-guid-a98643f8-7fff-9dd6-1624-71af243a5103\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"20483359\" _msthash=\"201\">霞海城隍夫人</span></span>",
+          "text": "<span id=\"docs-internal-guid-6b3afc42-7fff-f5dd-9e0a-4067b7f1cac6\"><p dir=\"ltr\" style=\"line-height:1.3800000000000001;margin-top:12pt;margin-bottom:12pt;\" _msttexthash=\"1030546621\" _msthash=\"203\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\">霞海城隍夫人</span><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"844297259\" _msthash=\"204\">奉祀始於清光緒年間，象徵柔性守護與陰陽平衡。信眾常祈求婚姻和諧，傳說御夫鞋能助改善伴侶關係，是極具人情故事與信仰溫度的神位之一。</span></p></span>"
+        }
+      ]
+    },
+    {
+      "id": "4-3",
+      "name": "側廳3菩薩",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.4234075422288157,
+          "pitch": 0.5288162992563734,
+          "rotation": 0,
+          "target": "5-1jpg"
+        },
+        {
+          "yaw": 2.4701817060781934,
+          "pitch": 0.5828765385238519,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08395594007651752,
+          "pitch": 0.556790430397756,
+          "title": "<span id=\"docs-internal-guid-e6d96933-7fff-9c05-47be-47a5be5831a1\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"9858914\" _msthash=\"360\">佛祖龕</span></span>",
+          "text": "<span id=\"docs-internal-guid-e0b597fe-7fff-f01e-df20-de9c24d04f54\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"1031414436\" _msthash=\"361\">佛祖龕主祀千手千眼觀音，並配祀達摩祖師、目連尊者、地藏王菩薩、彌勒佛、土地公與關帝爺，展現霞海廟融合佛道儒三教精神，祈求智慧、平安與福德。</span></span>"
+        }
+      ]
+    },
+    {
+      "id": "5-1jpg",
+      "name": "側廳1jpg",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.235298604610339,
+          "pitch": 0.4184407626242965,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.09349848191898857,
+          "pitch": 0.5193485113704028,
+          "rotation": 0,
+          "target": "3-1_2"
+        },
+        {
+          "yaw": 0.922158600809535,
+          "pitch": 0.517626128243382,
+          "rotation": 0,
+          "target": "4-3"
+        },
+        {
+          "yaw": 1.6930104865897766,
+          "pitch": 0.4678163594676441,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.3742458535336795,
+          "pitch": 0.49726088602744767,
+          "rotation": 0,
+          "target": "6-2_2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-2_2",
+      "name": "正廳2_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1906338766800104,
+          "pitch": 0.5978771419167401,
+          "rotation": 0,
+          "target": "5-1jpg"
+        },
+        {
+          "yaw": -3.0747321646592347,
+          "pitch": 0.3158440093816566,
+          "rotation": 0,
+          "target": "7-1_1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.02167576162317353,
+          "pitch": 0.028603692325415153,
+          "title": "<span id=\"docs-internal-guid-e737315c-7fff-2e5e-9e5b-e24c4f4a618a\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"29759938\" _msthash=\"1363\">正殿_霞海城隍爺</span></span>",
+          "text": "<span id=\"docs-internal-guid-4d1ba607-7fff-9975-63c9-9e4ca7f84318\"><span style=\"font-size: 12pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"887374124\" _msthash=\"1364\">正殿供奉霞海城隍爺，主掌陰陽、生死與善惡賞罰。另有兩尊軟身神像，分列供桌兩側，分別用於遶境與建醮，展現地方信仰儀式的莊嚴精神。</span></span>"
+        },
+        {
+          "yaw": -0.2756226419439649,
+          "pitch": 0.14530464540169774,
+          "title": "<span id=\"docs-internal-guid-718263c7-7fff-c4cd-d73f-465314ed47ab\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"10932025\" _msthash=\"1469\">月下老人</span></span>",
+          "text": "<span id=\"docs-internal-guid-2c8ea90f-7fff-fb97-c325-efc4a474e391\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"790679461\" _msthash=\"1470\">霞海月下老人神像創於 1971 年，靈驗事蹟廣為流傳，香客絡繹不絕。信徒以紅線與喜糖祈願，盼望牽起真誠姻緣、結好緣與善緣。</span></span>"
+        }
+      ]
+    },
+    {
+      "id": "7-1_1",
+      "name": "正廳1_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.19867587798491648,
+          "pitch": 0.2981008069756754,
+          "rotation": 0,
+          "target": "6-2_2"
+        },
+        {
+          "yaw": -3.111889451580387,
+          "pitch": 0.35515751421769437,
+          "rotation": 0,
+          "target": "8-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3670844490008882,
+          "pitch": 0.742564632986852,
+          "title": "<span id=\"docs-internal-guid-4cae2fb8-7fff-2834-02ae-2709ab75a915\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-weight: 700; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"4397692\" _msthash=\"1314\">拜亭</span></span>",
+          "text": "<span id=\"docs-internal-guid-b30033ab-7fff-7768-1904-36d6430dad7e\"><span style=\"font-size: 11pt; font-family: Arial, sans-serif; background-color: transparent; font-variant-numeric: normal; font-variant-east-asian: normal; font-variant-alternates: normal; font-variant-position: normal; font-variant-emoji: normal; vertical-align: baseline; white-space-collapse: preserve;\" _msttexthash=\"677714817\" _msthash=\"1315\">&nbsp;拜亭是進入廟宇前的重要空間，信徒會在此點香、祈願與靜心，是與神明對話前的準備區域，也是信仰情緒的轉換節點。</span></span>"
+        }
+      ]
+    },
+    {
+      "id": "8-2",
+      "name": "外景2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.552697597758657,
+          "pitch": 0.3150697928286643,
+          "rotation": 0,
+          "target": "9-1"
+        },
+        {
+          "yaw": 2.674545422843371,
+          "pitch": 0.20625620270826417,
+          "rotation": 0,
+          "target": "7-1_1"
+        },
+        {
+          "yaw": -2.5876277685858007,
+          "pitch": 0.3627918584178822,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-1",
+      "name": "外景1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 2.4688961663098503,
+        "pitch": 0.2291553741611434,
+        "fov": 1.319729308463192
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3254081196295022,
+          "pitch": 0.22707864690348423,
+          "rotation": 0,
+          "target": "8-2"
+        },
+        {
+          "yaw": -2.6903070774072226,
+          "pitch": 0.46120028381318257,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
