531bc7389ef59dea5340473410395f76a428c0de
LAI-wen <gigilai1688@gmail.com>
2025-05-18 19:19:20 +0800

    add praying nav vr

---
+++ praying/data.js
@@ -0,0 +1,577 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1金紙鋪",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 2.3657581090257374,
+        "pitch": 0.2818457131507799,
+        "fov": 1.183952773355305
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.725139897522876,
+          "pitch": 0.3598315797506153,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.261178825218728,
+          "pitch": 0.26876716544430224,
+          "title": "步驟1：備供品",
+          "text": "於金紙鋪備妥金紙與香。"
+        }
+      ]
+    },
+    {
+      "id": "1-2",
+      "name": "2取供品",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 0.18372761206555843,
+        "pitch": 0.424368933469367,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.623188598351069,
+          "pitch": 0.3392278113242284,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.18372761206555843,
+          "pitch": 0.424368933469367,
+          "title": "步驟2：取供品",
+          "text": "首次參拜者可洽詢【辦事處】取得供品(喜糖、鉛錢、紅線)。"
+        }
+      ]
+    },
+    {
+      "id": "2-3",
+      "name": "3點香處",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.19210907457905613,
+        "pitch": 0.29246760485492906,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.492791537903532,
+          "pitch": 0.20519556788009652,
+          "rotation": 0,
+          "target": "3-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2734022913012666,
+          "pitch": 0.37707889992297794,
+          "title": "步驟3：點香",
+          "text": "於點香處點香，三炷香請完全點燃。"
+        }
+      ]
+    },
+    {
+      "id": "3-4",
+      "name": "4拜天公",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.705181744843957,
+          "pitch": 0.17520882224288314,
+          "rotation": 0,
+          "target": "4-5-1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.25054224426173377,
+          "pitch": 0.2024241744766293,
+          "title": "步驟4：拜天公",
+          "text": "面朝外拜天公，自我介紹並祈求平安;勿插香並將香帶至辦事處。"
+        }
+      ]
+    },
+    {
+      "id": "4-5-1-",
+      "name": "5-1 領供品",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.742365891591474,
+        "pitch": 0.41565082211809923,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.10545112726633477,
+          "pitch": 0.2963800818612832,
+          "rotation": 0,
+          "target": "5-5-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.6592057599684669,
+          "pitch": 0.4037447116124149,
+          "title": "步驟5-1：領取供品",
+          "text": "至辦事處櫃台領取供品。"
+        }
+      ]
+    },
+    {
+      "id": "5-5-2",
+      "name": "5-2拜月老",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2597293979627437,
+          "pitch": 0.440298499604042,
+          "rotation": 0,
+          "target": "6-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.054485183832685635,
+          "pitch": 0.20825240478942852,
+          "title": "步驟5-2：拜城隍爺、月老及眾神",
+          "text": "面對城隍爺、月老及眾神，簡單自我就紹並祈求早日如願，並許願若姻緣有成後會拿喜餅前來答謝。"
+        }
+      ]
+    },
+    {
+      "id": "6-6",
+      "name": "6義勇公",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.25838862932585904,
+        "pitch": 0.3992869088266975,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0925523415320386,
+          "pitch": 0.38676055623171024,
+          "rotation": 0,
+          "target": "7-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.11234059590778855,
+          "pitch": 0.42344764787242184,
+          "title": "步驟6：拜義勇公",
+          "text": "至右側偏殿參拜義勇公:使小人遠離，諸事順利。"
+        }
+      ]
+    },
+    {
+      "id": "7-7",
+      "name": "7城隍夫人",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3093624042340313,
+          "pitch": 0.2623390069595217,
+          "rotation": 0,
+          "target": "8-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07633841880215186,
+          "pitch": 0.1771200040841414,
+          "title": "步驟7：拜城隍夫人",
+          "text": "參拜城隍夫人:保佑家庭幸福，夫妻和諧。"
+        }
+      ]
+    },
+    {
+      "id": "8-8",
+      "name": "8菩薩",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.1384714337100359,
+        "pitch": 0.13096721556252788,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.496602048590728,
+          "pitch": 0.44841544595397664,
+          "rotation": 0,
+          "target": "9-9"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.07287794004670012,
+          "pitch": 0.32515598916321764,
+          "title": "步驟8：拜菩薩",
+          "text": "參拜菩薩:祈求聰明長智慧，心境平和。"
+        }
+      ]
+    },
+    {
+      "id": "9-9",
+      "name": "9天公爐插香",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.2989484005803025,
+        "pitch": 0.06798768364638264,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.808378481903037,
+          "pitch": 0.1260824134242,
+          "rotation": 0,
+          "target": "10-10"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.2989299775768419,
+          "pitch": 0.06798768364638264,
+          "title": "步驟9：插香",
+          "text": "在天公爐插上三炷香;鉛錢與紅絲線在天公爐中順時鐘繞三圈，隨後放入皮夾妥善保存。"
+        }
+      ]
+    },
+    {
+      "id": "10-10",
+      "name": "10放喜糖",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -0.20802527727914466,
+        "pitch": 0.5048407928656857,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.55595853703535,
+          "pitch": 0.4920134997916108,
+          "rotation": 0,
+          "target": "11-11"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3263767270904392,
+          "pitch": 0.7950057543618705,
+          "title": "步驟十：拿喜糖",
+          "text": "供品內的喜糖可留下，供廟方煮平安茶分享大眾，廣結善緣。"
+        }
+      ]
+    },
+    {
+      "id": "11-11",
+      "name": "11放金紙",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 960,
+      "initialViewParameters": {
+        "yaw": 0.13638053038389053,
+        "pitch": 0.8799118480273496,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.354882802982452,
+          "pitch": 0.4945530484963552,
+          "rotation": 0,
+          "target": "12--12"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.029866841088718843,
+          "pitch": 0.7853040819753332,
+          "title": "步驟11：放金紙",
+          "text": "參拜過的金紙，可放入指定金紙箱中，本廟會代為集中焚燒。"
+        }
+      ]
+    },
+    {
+      "id": "12--12",
+      "name": " 12平安茶",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": 1.1715102512775335,
+        "pitch": 0.33586257086241744,
+        "fov": 1.3472704375743858
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.1294489624720736,
+          "pitch": 0.19030377129493736,
+          "title": "步驟12：平安茶",
+          "text": "吃喜糖與喜餅，喝平安茶(限量供應)，至此完成參拜順序。"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
